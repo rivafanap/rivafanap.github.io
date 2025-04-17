d62253a14b8ec2dd21e6ba6fe23c8c28594852ff
rivafanap <cursoadsfanap@gmail.com>
2025-04-16 21:23:34 -0300

    Update arquivo.js

--- arquivo.js
+++ arquivo.js
@@ -1,3 +1,17 @@
+let display = document.getElementById("display");
+
 function adicionarValor(valor) {
     display.value += valor;
+}
+
+function limpar() {
+    display.value = "";
+}
+
+function calcular() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
+    }
 }
