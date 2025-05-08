bf004eeeb043aab51f018ed2b8087dc091d2e581
rivafanap <cursoadsfanap@gmail.com>
2025-05-08 20:33:50 -0300

    Update script.js

--- numberone/script.js
+++ numberone/script.js
@@ -1,9 +1,9 @@
+// Rolagem suave
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         const targetId = this.getAttribute('href').substring(1);
         const targetSection = document.getElementById(targetId);
-        
         if (targetSection) {
             targetSection.scrollIntoView({
                 behavior: 'smooth',
