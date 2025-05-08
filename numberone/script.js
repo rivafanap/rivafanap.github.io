eb99a6fe61f4a11291ccd1fe44d90247098d08d9
rivafanap <cursoadsfanap@gmail.com>
2025-05-08 15:10:12 -0300

    Update script.js

--- numberone/script.js
+++ numberone/script.js
@@ -1,1 +1,15 @@
+// Rolagem suave para as seções
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
 
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
